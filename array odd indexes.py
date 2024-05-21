9edeb1a18431970def3dff34b4beb6ab86ac0dd1
haileleulz <hailewolfe1189@gmail.com>
2024-05-21 06:25:59 +0300

    array exercises

--- array odd indexes.py
+++ array odd indexes.py
@@ -1,13 +1,14 @@
-#  Write a program that prints only the elements at odd indices in an array.
+# Write a program that prints only the elements at odd indices in an array.
 
 def odd_index(numb):
-    numb = list(map(int,numb.split(",")))
-    odd_ind = []
-    for x in range(1,len(numb) + 1, 2):
-        odd_ind.append(x)
-    return odd_ind
+    numb = numb.split(",")
+    result = []
+    for x in range(len(numb)):
+        if x % 2 != 1:
+            result.append(numb[x])
+    return result
 
 
-user_input = input("please input a list : ")
+user_input = input("Please input a list: ")
 y = odd_index(user_input)
 print(y)
